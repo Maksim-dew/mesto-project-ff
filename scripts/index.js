7970508f812dd1c6f08af9684d16d1655f0377f5
Maksim-dew-xiaomi <maksim.medvedev0307@mail.ru>
2024-03-21 14:50:29 +0300

    Работа готова

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard(cardData, deleteCallback) {
+  const cardTemplate = document.querySelector('#card-template'); 
+  const cardElement = cardTemplate.content.querySelector('.places__item').cloneNode(true); 
+
+  const cardImage = cardElement.querySelector('.card__image'); 
+  cardImage.src = cardData.link; 
+
+  const cardTitle = cardElement.querySelector('.card__title'); 
+  cardTitle.textContent = cardData.name; 
+
+  const deleteButton = cardElement.querySelector('.card__delete-button'); 
+  deleteButton.addEventListener('click', function() { 
+    deleteCallback(cardElement); 
+  });
+
+  return cardElement;
+}
+
+function deleteCard(cardElement) {
+  cardElement.remove(); 
+}
+
+const placesList = document.querySelector('.places__list'); 
+
+initialCards.forEach(function(cardData) { 
+  const cardElement = createCard(cardData, deleteCard); 
+  placesList.appendChild(cardElement); 
+});
